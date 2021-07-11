86cf9d345cf39f5ea0c7da4cd4b55c4fe8a3d40b
Marko Polietaiev <markopolietaiev@gmail.com>
2021-07-10 20:38:11 -0700

    added tab bar controller

---
+++ Diploma/ViewController/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Diploma
+//
+//  Created by Marko Polietaiev on 2021-07-10.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
